9534f264f6516e06544eccafa4dac54d41a272e0
Tola <107399331+meet-tola@users.noreply.github.com>
2023-07-21 06:09:48 -1100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,7 @@ const navbar = document.querySelector("[data-navbar]");
 const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
 const navbarLinks = document.querySelectorAll("[data-nav-link]");
 
+
 const navToggleFunc = function () {
   navToggleBtn.classList.toggle("active");
   navbar.classList.toggle("active");
@@ -33,4 +34,29 @@ const header = document.querySelector("[data-header]");
 window.addEventListener("scroll", function () {
   window.scrollY >= 10 ? header.classList.add("active")
     : header.classList.remove("active");
+});
+
+/**
+ * video play-pause
+ */
+
+document.addEventListener("DOMContentLoaded", function () {
+  const video = document.getElementById("video");
+  const playPauseBtn = document.getElementById("playPauseBtn");
+
+  playPauseBtn.addEventListener("click", function () {
+    if (video.paused) {
+      video.play();
+    } else {
+      video.pause();
+    }
+  });
+
+  video.addEventListener("play", function () {
+    playPauseBtn.classList.add("playing");
+  });
+
+  video.addEventListener("pause", function () {
+    playPauseBtn.classList.remove("playing");
+  });
 });
